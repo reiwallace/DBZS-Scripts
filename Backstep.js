1bca25712ca8b7bc5f440d0d2828e15378624c6f
Reina W <reiwallace@yahoo.com>
2025-02-10 13:28:15 +0000

    changed targeting to be more consistent, changed variables to be more clear

--- Backstep.js
+++ Backstep.js
@@ -7,22 +7,21 @@ function backstep(e) { // Make npc backstep but move towards center if it hits a
     function getDirection(npc, x, z) { // Get direction to coordinates - Function by riken
         return Math.atan2(npc.getZ()-z, npc.getX()-x)
     }
-
-    var n = e.npc;
-    var tar = n.getSurroundingEntities(10, 1)[0];
-    if(tar != null) {
-        var tarLooking = tar.getLookVector();
+    var npc = e.npc;
+    var target = npc.getAttackTarget();
+    if(target != null) {
+        var targetLookVector = target.getLookVector();
     
-        if(n.world.rayCastBlock([n.x, n.y, n.z], [tarLooking.XD, 0, tarLooking.ZD], speed * 6, true, false, false) != null) { // Raycast to check if block in path
+        if(npc.world.rayCastBlock([npc.x, npc.y, npc.z], [targetLookVector.XD, 0, targetLookVector.ZD], speed * 6, true, false, false) != null) { // Raycast to check if block in path
             // Backstep to middle
-            var angle = getDirection(n, arenaCenter[0], arenaCenter[2]);
+            var angle = getDirection(npc, arenaCenter[0], arenaCenter[2]);
             var x = -Math.cos(angle) * speed; 
             var z = -Math.sin(angle) * speed;
         } else { // Backstep away from player
-            var angle = getDirection(n, tar.getX(), tar.getZ());
+            var angle = getDirection(npc, target.getX(), target.getZ());
             var x = Math.cos(angle) * speed; 
             var z = Math.sin(angle) * speed;
         }
-        n.setMotion(x, height, z);
+        npc.setMotion(x, height, z);
     }
 }
