7e83d8950065a356bda94dbb80c85db4bc383ea9
Reina Wallace <reiwallace@yahoo.com>
2025-02-18 10:29:03 +0000

    updated for window resizing

--- OverlaySpeak.js
+++ OverlaySpeak.js
@@ -1,7 +1,7 @@
 function speak(player, text, color, size, speakID) { // Place speech overlay on player's screen
     var speechOverlay = API.createCustomOverlay(speakID); // Create overlay with id
-    var x = 480 - Math.floor((text.length) * 2.5) * size; // Calculate centre position
-    var y = 246 - Math.floor(size * 6.5);
+    var x = player.getScreenSize().getWidth()/4 - Math.floor((text.length) * 2.5) * size; // Calculate centre position
+    var y = player.getScreenSize().getHeight()/4 - Math.floor(size * 6.5);
     speechOverlay.addLabel(1, text, x, y, 0, 0, color); // Add label in the middle of the screen with the given color
     speechOverlay.getComponent(1).setScale(size); // Resize the label
     player.showCustomOverlay(speechOverlay); // Place the overlay on the player's screen
