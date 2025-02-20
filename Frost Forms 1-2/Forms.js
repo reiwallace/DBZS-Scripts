247d99f4867cd548c69115d0a1e056fdd745efde
Reina <reiwallace@yahoo.com>
2025-02-20 20:51:34 +0000

    added temp variables and form detection

--- Frost Forms 1-2/Forms.js
+++ Frost Forms 1-2/Forms.js
@@ -1,22 +1,24 @@
 var baseDam = 10; // Base melee damage
 var baseRegen = 10; // Base health regen
-var formMulti = 1.5; // Form damage multiplier
-var arenaCenter = [-212, 56, -777]; // Spot npc is tped to when transforming
+var movementSpeed = 7; // Boss movement speed
+var formMulti = 1.5; // Form damage and regen multiplier
+var arenaCenter = [-258, 56, -843]; // Spot npc is tped to when transforming
 var resetTime = 600; // Time after last being damage that the npc resets
+var transformPercent = 0.55; // Percent health the boss transforms at 
+var formName = "FrostTransformation"; // Form used to give frost his aura
 
 var transformText1 = "&b&lTransform text1"; // Line spoken at the beginning of transformation
 var transformText2 = "&b&lTransform text2"; // Line spoken in the middle of transformation
 var transformText3 = "&b&lTransform text3"; // Line spoken at the end of transformation
 
-var increment = 0;
+var count = 0;
 
 function init(e) {
     var npc = e.npc;
-    var dbcNpc = DBCAPI.getDBCDisplay(npc);
-    dbcNpc.transform(DBCAPI.getForm("FrostTransformation")); // Ensure right aura is enabled
-    dbcNpc.setEnabled(false);
-    changeForm(npc, baseDam, baseRegen, 5, 1); // Set to base form
-    npc.timers.forceStart(10, 20, true);
+    DBCAPI.getDBCDisplay(npc).transform(DBCAPI.getForm(formName)); // Ensure right aura is enabled
+    npc.setTempData("Movement Speed", movementSpeed);
+    npc.setTempData("Reset Time", resetTime)
+    reset(npc); // Reset timers and form
 }
 
 function timer(e) { 
@@ -25,42 +27,43 @@ function timer(e) {
     if(id == 10) { // Reset boss after set amount of time
         reset(npc);
     } else if(id == 11) { // Transformation Animation
-        if(!npc.timers.has(12)) {
+        if(!npc.timers.has(12)) { // Stop timer once transformation is complete
             npc.timers.stop(11);
         }
         if(npc.getTempData("Form") != 2) {
             npc.setSize(npc.getSize() + 1);  // Grow
+            npc.playSound("jinryuudragonbc:DBC3.force", 100, 1);
         }
-        if(increment == 1) { // Say second line halfway through transforming
+        if(count == 1) { // Say second line halfway through transforming
             npc.say(transformText2);
         }
-        increment++;
+        count++;
     } else if(id == 12) { // End transformation
         DBCAPI.getDBCDisplay(npc).setEnabled(false); // Disable aura
-        npc.setSpeed(7); // Let boss move again
+        npc.setSpeed(movementSpeed); // Let boss move again
         npc.setFaction(2); // Make boss targetable
         npc.say(transformText3);
         changeForm(npc, baseDam * formMulti, baseRegen * formMulti, 6, 2); // Change model
+        npc.setTempData("Transforming", false);
         lightningSpam(npc);
     }
 }
 
 function damaged(e) { // Hp breakpoint detection
     var npc = e.npc;
-    npc.timers.forceStart(10, resetTime, false);
-    var maxHp = npc.getMaxHealth();
-    var currentHp = npc.getHealth();
-    if(currentHp < maxHp * 0.50 && npc.getTempData("Form") == 1) { // P2 transformation
+    npc.timers.forceStart(10, resetTime, false); // Reset if not hit in a set amount of time
+    if(npc.getHealth() < npc.getMaxHealth() * transformPercent && npc.getTempData("Form") == 1) { // P2 transformation
         npc.say(transformText1);
         npc.setX(arenaCenter[0]);
         npc.setY(arenaCenter[1]);
         npc.setZ(arenaCenter[2]);
         npc.setSpeed(0); // Make npc stand still
         npc.setFaction(0); // Make npc untargetable
-        increment = 0;
+        count = 0;
         DBCAPI.getDBCDisplay(npc).setEnabled(true); // Enable aura
-        npc.timers.forceStart(11, 50, true);
-        npc.timers.forceStart(12, 200, false);
+        npc.setTempData("Transforming", true);
+        npc.timers.forceStart(11, 50, true); // Grow timer
+        npc.timers.forceStart(12, 200, false); // End transformation timer
     }
 }
 
@@ -75,14 +78,17 @@ function kills(e) { // Reset on killing the player
 function reset(npc) { // Revert to first form and clear timers
     changeForm(npc, baseDam, baseRegen, 5, 1);
     DBCAPI.getDBCDisplay(npc).setEnabled(false);
-    npc.setSpeed(7);
+    npc.setTempData("Transforming", false);
+    npc.setSpeed(movementSpeed);
     npc.setFaction(2);
-    npc.timers.clear();
+    for(i = 2; i < 12; i++) { // Resetting timers manually to keep poison reset
+        npc.timers.stop(i);
+    }
 }
 
 function changeForm(npc, melee, regen, size, form) { // Change npc's stats, model and temp data
     npc.setMeleeStrength(melee);
-    npc.setHealthRegen(regen);
+    npc.setCombatRegen(regen);
     npc.setSize(size);
     npc.setTempData("Form", form);
     npc.getModelData().setEntity("JinRyuu.DragonBC.common.Npcs.EntityFrost" + form);
