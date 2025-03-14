4928f5d85e1e77c487431d4b72947c885e3d4b9f
Reina <reiwallace@yahoo.com>
2025-03-14 15:37:29 +0000

    finished homing ki attack

--- Caulifa & Kale fight/Caulifla.js
+++ Caulifa & Kale fight/Caulifla.js
@@ -2,15 +2,21 @@
 var kaleNpcName = "Kale"; // Name of accompanying kale npc
 var abilityInterval = 100; // Time between abilities in ticks
 
-var homingKiDamage; // Damage of homing ki attack
+var homingKiShots = 8; // Number of homing ki shots to fire
+var homingKiDamage = 100; // Damage of homing ki attack
 
 var KALE;
-var TARGET;
+var TARGET_ONE;
+var TARGET_TWO;
+var COUNT;
 var TO_RESET;
 var HOMING_KI_ENTITIES;
 
 // Timers
-var CHOOSE_ABILITY = 0;
+var RESET_TIMER = 0;
+var CHOOSE_ABILITY = 1;
+var FIRE
+var HOMING_KI_TIMER = 3;
 
 var HOMING_KI = 0;
 
@@ -31,6 +37,19 @@ function timer(event) {
     switch(event.timer) {
         case(CHOOSE_ABILITY):
             chooseAbility(npc);
+            break;
+        case(HOMING_KI_TIMER):
+            if(count > 8) { 
+                npc.timers.stop(HOMING_KI_TIMER);
+            }
+            for(i = 0; i < HOMING_KI_ENTITIES.length; i++) {
+                if(i < homingKiShots/2 || TARGET_TWO == null) {
+                    homeKi(HOMING_KI_ENTITIES[i], TARGET_ONE);
+                } else {
+                    homeKi(HOMING_KI_ENTITIES[i], TARGET_TWO);
+                }
+            }
+            COUNT++;
             break;
     }
 }
@@ -65,7 +84,11 @@ function chooseAbility(npc)
 {
     switch(getRandomInt(0, 1)){
         case(HOMING_KI):
-            
+            HOMING_KI_ENTITIES = new Array();
+            for(i = 0; i < homingKiShots; i++) {
+                fireHomingKi(npc);
+            }
+            npc.timers.forceStart(HOMING_KI_TIMER, 5, true);
             break;
         case(2):
 
@@ -79,11 +102,37 @@ function chooseAbility(npc)
 function fireHomingKi(npc)
 {
     npc.executeCommand("/dbcspawnki 1 0 " + homingKiDamage + " 0 4 10 1 100 " + npc.x + " " + npc.y + " " + npc.z + "");
-    var kiScan = npc.getSurroundingEntities(20);
+    var kiScan = npc.getSurroundingEntities(10);
     for(i = 0; i < kiScan.length; i++) {
         if(kiScan[i].getType() == 0 && HOMING_KI_ENTITIES.indexOf(kiScan[i]) < 0) {
             HOMING_KI_ENTITIES.push(kiScan[i])
             kiScan[i].setMotion(0, 0, 0);
         }
     }
+}
+
+/** Sets a ki attack's motion towards a target
+ * @param {*} ki - Ki attack to change motion of
+ * @param {*} target - Target to head towards
+ */
+function homeKi(ki, target)
+{
+    if(ki != null) {
+        /*  Directional math
+         * 
+         */
+        ki.setMotion(x, y, z);
+    }
+}
+
+/** Clear timers and delete extra ki
+ * @param {ICustomNpc} npc - Npc to reset
+ */
+function reset(npc) {
+    for(i = 0; i < HOMING_KI_ENTITIES.length; i++){
+        if(HOMING_KI_ENTITIES[i] != null) {
+            HOMING_KI_ENTITIES[i].despawn();
+        }
+    }
+    npc.timers.clear();
 }
