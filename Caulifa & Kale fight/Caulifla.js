0176335bb933d13020ed6eb7e42668c4a75f20c8
Reina <reiwallace@yahoo.com>
2025-03-14 14:52:37 +0000

    set up ability choice timer

--- Caulifa & Kale fight/Caulifla.js
+++ Caulifa & Kale fight/Caulifla.js
@@ -1,8 +1,13 @@
 // Changeables
 var kaleName = "Kale"; // Name of accompanying kale npc
+var abilityInterval = 100; // Time between abilities in ticks
 
 var KALE;
 var TARGET;
+var TO_RESET;
+
+// Timers
+var CHOOSE_ABILITY = 0;
 
 function init(event) {
     var npc = event.npc;
@@ -15,10 +20,25 @@ function init(event) {
     }
 }
 
-function decideTargets(npc) {
-
+function timer(event) {
+    var npc = event.npc;
+    switch(event.timer) {
+        case(CHOOSE_ABILITY):
+            chooseAbility(npc);
+            break;
+    }
 }
 
-function setTarget(npc, target) {
-    npc.setAttackTarget(target);
+function meleeAttack(event)
+{ // Set target and begin reset timer on swing
+    var npc = event.npc;
+    TARGET = npc.getAttackTarget();
+    DBC_TARGET = TARGET.getDBCPlayer();
+    npc.timers.forceStart(10, npc.getTempData("Reset Time"), false); // Reset if doesn't melee a target for set time
+    if(TO_RESET.indexOf(TARGET) < 0) { // Add reset targets if not in array already
+        TO_RESET.push(TARGET);
+    }
+    if(!npc.timers.has(CHOOSE_ABILITY)) {
+        npc.timers.forceStart(CHOOSE_ABILITY, abilityInterval, true); // Start ability timer
+    }
 }
