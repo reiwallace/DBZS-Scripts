bac42c71b52b1da8da57f25f10855c32ee937a1f
Reina <reiwallace@yahoo.com>
2025-04-07 13:17:30 +0100

    moved particle motion to it's own folder and added two examples

---
+++ Particle Motion/MultiParticleExample.js
@@ -0,0 +1,92 @@
+var particlePath = "plug:textures/blocks/concrete_periwinkle.png";
+var newPoses = new Array();
+var newMotions = new Array();
+var duration = 0;
+
+function interact(event)
+{ // Starts particle movememnt on interacting with npc
+    var npc = event.npc
+    newPoses = [
+        [npc.x, npc.y + 3, npc.z],
+        [npc.x, npc.y + 4, npc.z],
+        [npc.x, npc.y + 5, npc.z],
+        [npc.x, npc.y + 6, npc.z],
+        [npc.x, npc.y + 7, npc.z]
+    ];
+    newMotions = [
+        [0.5, 0, 0.5],
+        [-0.5, 0, -0.5],
+        [0, 0, 0.5],
+        [0.5, 0, 0],
+        [0, 0, -0.5]
+    ];
+    duration = 0;
+    for(i = 0; i < newPoses.length; i++) {
+        particleDirectionChange(npc.world, particlePath, duration, newPoses[i][0], newPoses[i][1], newPoses[i][2], newMotions[i][0], newMotions[i][1], newMotions[i][2]);
+    }
+    npc.timers.forceStart(0, duration, true); // Start particle movement
+    npc.timers.forceStart(1, 200, false); // Stop particle movememnt
+}
+
+function timer(event)
+{
+    var npc = event.npc;
+    switch(event.id) {
+        case(0):
+            for(i = 0; i < newPoses.length; i++) {
+                newPoses[i] = getNewPos(newPoses[i], newMotions[i], duration); // Set new postion on global variable to be used for the next movement
+                newMotions[i] = [genRand(-1, 1, 3), 0, genRand(-1, 1, 3)] // Save motion as a global variable to be iterated on later
+                particleDirectionChange(npc.world, particlePath, duration, newPoses[i][0], newPoses[i][1], newPoses[i][2], newMotions[i][0], newMotions[i][1], newMotions[i][2]);
+            }
+            break;
+        case(1):
+            npc.timers.clear();
+            break;
+    }
+    
+}
+
+/** Generate a random decimal number
+ * @param {int} min - Minimum value to generate between 
+ * @param {int} max - Maximum value to generate between
+ * @param {int} decimalPlaces - Decimal places to round number to
+ * @returns {Double} - random number between the min and max values with decimalPlaces amount of decimal places
+ */
+function genRand(min, max, decimalPlaces)
+{  
+    var rand = Math.random()*(max-min) + min;
+    var power = Math.pow(10, decimalPlaces);
+    return Math.floor(rand*power) / power;
+}
+
+/** Spawns a particle at a specific coordinates with a specific motion
+ * @param {IWorld} world - World to spawn particle in
+ * @param {String} particlePath - Path of particle texture to use
+ * @param {int} timerDuration - Duration of the timer (I add +1 for timer delay just put in timer duration)
+ * @param {Double} x - Initial x position to spawn particle at
+ * @param {Double} y - Initial y position to spawn particle at
+ * @param {Double} z - Initial z position to spawn particle at
+ * @param {Double} mx - X motion of particle
+ * @param {Double} my - Y motion of particle
+ * @param {Double} mz - Z motion of particle
+ */
+function particleDirectionChange(world, particlePath, timerDuration, x, y, z, mx, my, mz)
+{
+    var particle = API.createParticle(particlePath);
+    particle.setPosition(x, y, z);
+    particle.setMaxAge(timerDuration + 1);
+    particle.setMotion(mx, my, mz, 0); // Gravity not supported
+    particle.spawn(world)
+}
+
+/** Calculates new position based on motion and duration
+ * @param {Double[]} oldPos - Old position to calculate new position from
+ * @param {Double[]} oldMotion - Motion of particle from old position
+ * @param {int} duration - How long particle was in motion for (1 is added to this in the function to account for timer delay)
+ * @returns {Double[]} - New position of particle calculated from motion and duration
+ */
+function getNewPos(oldPos, oldMotion, duration)
+{
+    // Calculates a new position assuming motion is full motion per tick e.g. if motion x was 1 in 10 ticks the particle travels 10 blocks in the x direction
+    return [oldPos[0] + oldMotion[0] * (duration + 1), oldPos[1] + oldMotion[1] * (duration + 1), oldPos[2] + oldMotion[2] * (duration + 1)];
+}
--- Particle Motion/ParticleMover.js
+++ Particle Motion/ParticleMover.js
@@ -1,8 +1,7 @@
-
 /** Spawns a particle at a specific coordinates with a specific motion
  * @param {IWorld} world - World to spawn particle in
  * @param {String} particlePath - Path of particle texture to use
- * @param {int} duration - Duration of the timer (I add +1 for timer delay just put in timer duration)
+ * @param {int} timerDuration - Duration of the timer (I add +1 for timer delay just put in timer duration)
  * @param {Double} x - Initial x position to spawn particle at
  * @param {Double} y - Initial y position to spawn particle at
  * @param {Double} z - Initial z position to spawn particle at
@@ -10,10 +9,11 @@
  * @param {Double} my - Y motion of particle
  * @param {Double} mz - Z motion of particle
  */
-function particleDirectionChange(world, particlePath, duration, x, y, z, mx, my, mz) {
+function particleDirectionChange(world, particlePath, timerDuration, x, y, z, mx, my, mz)
+{
     var particle = API.createParticle(particlePath);
     particle.setPosition(x, y, z);
-    particle.setMaxAge(duration + 1);
+    particle.setMaxAge(timerDuration + 1);
     particle.setMotion(mx, my, mz, 0); // Gravity not supported
     particle.spawn(world)
 }
@@ -21,10 +21,10 @@ function particleDirectionChange(world, particlePath, duration, x, y, z, mx, my,
 /** Calculates new position based on motion and duration
  * @param {Double[]} oldPos - Old position to calculate new position from
  * @param {Double[]} oldMotion - Motion of particle from old position
- * @param {int} duration - How long particle was in motion for
+ * @param {int} duration - How long particle was in motion for (1 is added to this in the function to account for timer delay)
  * @returns {Double[]} - New position of particle calculated from motion and duration
  */
-function getNewPos(oldPos, oldMotion, duration) {
-    // Calculates a new position assuming motion is full motion per tick e.g. if motion x was 1 in 10 ticks the particle travels 10 blocks in the x direction
-    return [oldPos[0] + oldMotion[0] * duration, oldPos[1] + oldMotion[1] * duration, oldPos[2] + oldMotion[2] * duration];
+function getNewPos(oldPos, oldMotion, duration)
+{ // Calculates a new position assuming motion is full motion per tick e.g. if motion x was 1 in 10 ticks the particle travels 10 blocks in the x direction
+    return [oldPos[0] + oldMotion[0] * (duration + 1), oldPos[1] + oldMotion[1] * (duration + 1), oldPos[2] + oldMotion[2] * (duration + 1)];
 }
