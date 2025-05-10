c1b9f7058b13fe8fb079839cd88f83cab12c162f
Reina <reiwallace@yahoo.com>
2025-05-10 18:22:55 +0100

    Implement deck class

---
+++ DeckRng.js
@@ -0,0 +1,38 @@
+/** Deck class.
+ * @constructor
+ * @param {int} size - Deck size - number of items to cycle through.
+ */
+function deck(size) {
+    this.maxSize = size;
+    this.deck = new Array(size);
+    this.fillDeck();
+}
+
+/** Get a random item from the deck
+ * @returns {int}
+ */
+deck.prototype.getRandom = function() {
+    if(this.deck.length < 1) this.reset();
+    return this.deck.splice(Math.floor(Math.random() * (this.deck.length - 0 )) + 0, 1);
+}
+
+/** Gets the next item from the deck
+ * @returns {int}
+ */
+deck.prototype.getNext = function() {
+    if(this.deck.length < 1) this.reset();
+    return this.deck.pop();
+}
+
+/** Resets the deck to its original size
+ */
+deck.prototype.reset = function() {
+    this.deck = new Array(this.maxSize);
+    this.fillDeck();
+}
+
+/** Fills the deck with numbers
+ */
+deck.prototype.fillDeck = function() {
+    for(i = 0; i < this.maxSize; i++) this.deck[i] = i;
+}
