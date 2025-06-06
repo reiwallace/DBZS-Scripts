3e8b4936352a44d20fcba1251b4b0d070a3d29b0
Reina <reiwallace@yahoo.com>
2025-06-06 21:11:53 +0100

    Implement progress bar

--- Granolah (Spar 1)/Granolah.js
+++ Granolah (Spar 1)/Granolah.js
@@ -21,11 +21,12 @@ var recoil3Name = "GranolahBarrageRight";
 
 // STANCE CHANGE CONFIG
 var GUARD_BREAKPOINTS = [0.25, 0.5, 0.75]; // Points to perform a stance change
-var STANCE_CHANGE_PARTICLE = "LARGE_SMOKE"; // IParticle or particle string to dispaly on stance change
+var STANCE_CHANGE_PARTICLE = "cloud"; // IParticle or particle string to dispaly on stance change
 var STANCE_CHANGE_BOOL = false; // if particle is a iParticle object or a string identifier(true for iParticle, false for string identifier)
-var STANCE_CHANGE_PARTICLE_COUNT = 20; // Number of particles generated on stance change
-var STANCE_CHANGE_PARTICLE_RADIUS = 4; // Radius to spawn particles around the npc
+var STANCE_CHANGE_PARTICLE_COUNT = 100; // Number of particles generated on stance change
+var STANCE_CHANGE_PARTICLE_RADIUS = 3; // Radius to spawn particles around the npc
 var STANCE_CHANGE_SOUND = "minecraft:ambient.weather.thunder"; // Sound to play to player on stance change
+var STANCE_CHANGE_MESSAGE = "&6Granolah shifts his stance!"
 
 // BOSS CONFIG
 var RESET_TIME = 600; // Number of ticks since player activity to reset
@@ -46,8 +47,42 @@ var QTE_TIMER = 3;
 guard.prototype.guardConfig = function()
 {
     // GUARD CONFIG
-    this.GUARD_SIZE = 10; // Size of guard
+    this.GUARD_SIZE = 25; // Size of guard
     this.GUARD_IFRAMES = 10; // Min ticks between guard hits
+
+    this.GUARD_BREAK_MESSAGE = "&2&lGranolah's guard falls!";
+}
+
+// GUARD BAR CONFIG
+progressBar.prototype.config = function() {
+    // COMPONENT IDS
+    this.OVERLAY_ID = 200;
+    this.BORDER_ID = 1;
+    this.BAR_ID = 2;
+    this.TICK_INITIAL_ID = 3;
+    this.TEXT_ID = 50;
+    this.SHADOW_ID = 51;
+    
+    // POSITIONING
+    this.x = 480;
+    this.y = 30;
+
+    // COLOUR CONFIG
+    this.BORDER_COLOUR = 1; // Border colour - takes a decimal colour (https://www.mathsisfun.com/hexadecimal-decimal-colors.html)
+    this.BAR_COLOUR = 13466141; // Bar colour - takes a decimal colour (https://www.mathsisfun.com/hexadecimal-decimal-colors.html)
+    this.BAR_WIDTH = 250; // Width of bar in pixels
+    this.BAR_HEIGHT = 7; // Height of bar in whatever the game feels like
+
+    // TICK CONFIG
+    this.TICK_COLOUR = 0; // Tick colour - takes a decimal colour (https://www.mathsisfun.com/hexadecimal-decimal-colors.html)
+    this.TICK_THICKNESS = 1;
+
+    // TEXT CONFIG
+    this.TEXT = "Guard Level: "; // Text displayed for the bar
+    this.TEXT_POSITION = -1; // Set to -1 to position above bar +1 for below
+    this.TEXT_COLOUR = 16777215; // Text colour
+    this.TEXT_SIZE = 1;
+    this.SHADOW_COLOUR = 0; // Colour of shadow behind text
 }
 
 // QTE CONFIG
@@ -92,6 +127,7 @@ var npcAnimHandler;
 var playerAnimHandler;
 var qte;
 var target;
+var targets = [];
 var recoil = null;
 var count = 0;
 
@@ -106,6 +142,7 @@ function timer(event)
         case(BACKSTEP):
             // Perform backstep and start backshots timer
             if(qte.isPerformingQTE()) return;
+            
             backstep(npc, target, BACKSTEP_SPEED, BACKSTEP_HEIGHT, ARENA_CENTRE);
             recoil = null;
             count = 0;
@@ -153,6 +190,10 @@ function init(event)
 {
     var npc = event.npc;
     npc.timers.clear();
+    for(var i = 0; i < targets.length; i++) {
+        if(targets[i]) npcGuard.getGuardDisplay().removeBar(targets[i]);
+    }
+    targets = new Array();
     npc.setSpeed(NPC_SPEED);
 
     // Initalise guard breakpoints
@@ -162,7 +203,7 @@ function init(event)
     npcAnimHandler = new animationHandler(npc);
     npcAnimHandler.removeAnimation();
     qte = new qteHandler(npc, npcAnimHandler, ARENA_CENTRE, QTE_TIMER, NPC_SPEED);
-    npcGuard = new guard(npc, npcAnimHandler, npc.getAggroRange(), qte);
+    npcGuard = new guard(npc, npcAnimHandler, npc.getAggroRange(), qte, GUARD_BREAKPOINTS);
 }
 
 function target(event)
@@ -181,6 +222,7 @@ function meleeAttack(event)
 function damaged(event)
 { 
     var npc = event.npc;
+    var player = event.source;
 
     // Begin reset timer on damaged
     npc.timers.forceStart(RESET, RESET_TIME, false);
@@ -190,11 +232,16 @@ function damaged(event)
     if(npcGuard.isGuardBarEmpty()) return;
     event.setDamage(0);
     npcGuard.damageGuard(GUARD_DAMAGE);
+    if(isValidPlayer(player)) {
+        npcGuard.getGuardDisplay().displayBar(player);
+        targets.push(player);
+    }
 
     // Trigger stance change if guard brought below breakpoints
     var guardPercent = npcGuard.getGuardLevel() / npcGuard.getInitialGuard();
     for(var i = 0; i < GUARD_BREAKPOINTS.length; i++) {
         if(guardPercent < GUARD_BREAKPOINTS[i] && !breakPoints[i]) {
+            breakPoints[i] = !breakPoints[i];
             stanceChange(npc, GUARD_BREAKPOINTS[i]);
             return;
         }
@@ -205,26 +252,11 @@ function killed(event)
 { // Reset if killed
     var npc = event.npc;
     var player = event.player;
-    if (player != null && player.getType() == 1) {
-        player.closeGui();
-        npc.timers.clear();
+    for(var i = 0; i < targets.length; i++) {
+        if(!targets[i]) return;
+        npcGuard.getGuardDisplay().removeBar(targets[i]);
+        targets[i].closeGui();
     }
-    reset(npc);
-}
-
-function kills(event)
-{ // Reset if killing a player and no other players around
-    var npc = event.npc;
-    var playerCheck = npc.getSurroundingEntities(npc.getAggroRange(), 1);
-    if(playerCheck.length < 1) reset;
-}
-
-/** Resets npc's timers and temp data
- * @param {ICustomNpc} npc - Npc to reset
- */
-function reset(npc)
-{
-    npc.reset();
     npc.timers.clear();
 }
 
@@ -248,6 +280,7 @@ function startTimers(npc)
 function backstep(npc, target, speed, height, arenaCenter)
 {
     if(target == null) return;
+
     var targetLookVector = target.getLookVector();
     // Boolean variable to check if the npc would hit a block from a backstep
     var wallCheck = npc.world.rayCastBlock(
@@ -255,7 +288,7 @@ function backstep(npc, target, speed, height, arenaCenter)
         [targetLookVector.XD, 0, targetLookVector.ZD], 
         speed * 6, true, false, false
     );
-    if(wallCheck != null) { // Raycast to check if block in path
+    if(wallCheck != null) { // If block is in path
         // Backstep to middle
         var angle = getDirection(npc, arenaCenter[0], arenaCenter[2]);
         var x = -Math.cos(angle) * speed; 
@@ -286,8 +319,6 @@ function getDirection(npc, x, z)
  */
 function stanceChange(npc, breakPoint)
 {
-    breakPoints[breakPoint] = true;
-    //npc.say("Stance change at " + GUARD_BREAKPOINTS[breakPoint] * 100 + "%");
     stanceChangeVisuals(
         npc, 
         STANCE_CHANGE_PARTICLE, 
@@ -297,9 +328,10 @@ function stanceChange(npc, breakPoint)
         STANCE_CHANGE_SOUND,
         target 
     );
+    if(target) target.sendMessage(STANCE_CHANGE_MESSAGE);
 
     // STAT CHANGES - Add more if more breakpoints are added
-    switch(breakPoint) {
+    switch(breakPoint) { // Switch breakpoint % e.g - breakPoint = 0.25, matches case GUARD_BREAKPOINTS[3]
         case(GUARD_BREAKPOINTS[0]):
 
             break;
@@ -376,6 +408,24 @@ function generateRandomPoints(cx, cy, cz, radius, count)
     return points
 }
 
+/**
+ * Checks if the given player is valid.
+ *
+ * A player is considered valid if:
+ * - The player object is not null.
+ * - The player's type is 1.
+ * - The player has a non-null DBCPlayer instance.
+ * - The player's mode is 0.
+ * - The player is not a DBC Fusion Spectator.
+ *
+ * @param {IPlayer} player - The player to validate.
+ * @returns {boolean} True if the player is valid, otherwise false.
+ */
+function isValidPlayer(player)
+{
+    return (player && player.getType() == 1 && player.getDBCPlayer() && player.getMode() == 0 && !player.getDBCPlayer().isDBCFusionSpectator())
+}
+
 // Guard Class ---------------------------------------------------------
 
 /** A guard bar that takes damage and performs a block animation
@@ -385,7 +435,7 @@ function generateRandomPoints(cx, cy, cz, radius, count)
  * @param {int} scanRange - Range to scan for players 
  * @param {qteHandler} qte - Quick time event to perform on guard break  
  */
-function guard(npc, npcAnimationHandler, scanRange, qte)
+function guard(npc, npcAnimationHandler, scanRange, qte, guardBreakPoints)
 {
     this.guardConfig();
     this.npc = npc;
@@ -394,6 +444,7 @@ function guard(npc, npcAnimationHandler, scanRange, qte)
     this.scanRange = scanRange;
     this.guardLevel = this.GUARD_SIZE;
     this.qte = qte;
+    this.guardDisplay = new progressBar(this.GUARD_SIZE, this.GUARD_SIZE, guardBreakPoints);
 }
 
 /** Set guard bar level
@@ -402,17 +453,15 @@ function guard(npc, npcAnimationHandler, scanRange, qte)
 guard.prototype.setGuardBar = function(value)
 {
     this.guardLevel = value;
+    this.guardDisplay.setBar(value);
     
     // Update player on guard status
-    var message = "";
-    if (this.guardLevel > 0) message = "GUARD LEVEL: " + this.guardLevel;
-    else {
-        message = "GUARD BROKEN";
-        this.qte.newQTE(target, new animationHandler(target));
-    } 
+    if (this.guardLevel > 0) return;
+    this.qte.newQTE(target, new animationHandler(target));
+
     var entities = this.npc.getSurroundingEntities(this.scanRange, 1);
     for (var i in entities) {
-        entities[i].sendMessage(message);
+        entities[i].sendMessage(this.GUARD_BREAK_MESSAGE);
     }
 }
 
@@ -439,6 +488,7 @@ guard.prototype.isGuardBarEmpty = function()
 
 guard.prototype.getInitialGuard = function() { return this.GUARD_SIZE; }
 guard.prototype.getGuardLevel = function() { return this.guardLevel; }
+guard.prototype.getGuardDisplay = function() { return this.guardDisplay; }
 
 // ---------------------------------------------------------
 
@@ -589,3 +639,74 @@ animationHandler.prototype.removeAnimation = function()
 }
 
 // ---------------------------------------------------------------------------
+
+/** progressBar constructor
+ * @param {Int} maxValue - Max value of bar
+ * @param {Int} initialValue - Initial value of bar
+ * @param {Double[]} breakPoints - An ARRAY of decimal values to place ticks at 
+ * @returns 
+ */
+function progressBar(maxValue, initialValue, breakPoints) 
+{
+    if(maxValue == null || initialValue == null || !breakPoints.constructor === Array) return;
+    this.config();
+    this.maxValue = maxValue;
+    this.breakPoints = breakPoints;
+    this.setBar(initialValue);
+}
+
+/** Creates the bar and sets it to a given value
+ * @param {Int} value - Value to set bar to 
+ */
+progressBar.prototype.setBar = function(value)
+{
+    // Create overlay
+    var barOverlay =  // Create overlay with id
+    this.barOverlay = API.createCustomOverlay(this.OVERLAY_ID);;
+
+    // Build bar border
+    var border = barOverlay.addLine(this.BORDER_ID, this.x - this.BAR_WIDTH/2, this.y, this.x + this.BAR_WIDTH/2, this.y);
+    border.setThickness(this.BAR_HEIGHT);
+    border.setColor(this.BORDER_COLOUR);
+
+    // Build bar itself
+    var barX1 = this.x - this.BAR_WIDTH/2 + 1;
+    var barX2 = barX1 + (this.BAR_WIDTH - 2) * value / this.maxValue;
+    var bar = barOverlay.addLine(this.BAR_ID, barX1, this.y - 1, barX2, this.y - 1);
+    bar.setThickness(this.BAR_HEIGHT - 2);
+    bar.setColor(this.BAR_COLOUR);
+
+    // Add ticks
+    for(var i = 0; i < this.breakPoints.length; i++) {
+        var tick = barOverlay.addLine(this.TICK_INITIAL_ID + i, barX1 + (this.BAR_WIDTH * this.breakPoints[i]), this.y - 1, barX1 + (this.BAR_WIDTH * this.breakPoints[i]), this.y - this.BAR_HEIGHT + 1);
+        tick.setColor(this.TICK_COLOUR);
+        tick.setThickness(this.TICK_THICKNESS);
+    }
+
+    // Add bar text
+    var text = this.TEXT + value;
+    var lx = this.x - Math.floor((text.length) * 2.5) * this.TEXT_SIZE; // Calculate centre position
+    var ly = this.y - Math.floor(this.TEXT_SIZE * 6.5) + 12 * this.TEXT_POSITION;
+    var shadowLabel = barOverlay.addLabel(this.TEXT_ID, text, lx, ly, 0, 0, this.SHADOW_COLOUR); // Add label in the middle of the screen with the given color
+    shadowLabel.setScale(this.TEXT_SIZE);
+    var textLabel = barOverlay.addLabel(this.SHADOW_ID, text, lx - 1, ly - 1, 0, 0, this.TEXT_COLOUR); // Add label in the middle of the screen with the given color
+    textLabel.setScale(this.TEXT_SIZE); 
+}
+
+/** Adds bar to a player's UI
+ * @param {IPlayer} player - Player to display bar to
+ */
+progressBar.prototype.displayBar = function(player)
+{
+    if(!player) return;
+    player.showCustomOverlay(this.barOverlay);
+    this.barOverlay.update(player);
+}
+
+/** Removes bar from a player's UI
+ * @param {IPlayer} player - Player to remove bar from
+ */
+progressBar.prototype.removeBar = function(player)
+{
+    if(player) player.closeOverlay(this.OVERLAY_ID); 
+}
--- Granolah (Spar 1)/QTEGlobal.js
+++
@@ -1,6 +0,0 @@
-function customGuiButton(event)
-{
-    // Passes quick time event if corret id
-    var qteHandler = event.player.getTempData("qteHandler");
-    if(qteHandler != null && event.id == qteHandler.getButtonId()) qteHandler.passQTE();
-}
