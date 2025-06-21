aa4d914c22f94fa8bd605103ad4805141330b37b
Reina <reiwallace@yahoo.com>
2025-06-21 10:57:03 +0100

    Implement getRandom function

--- ZS Scripting Library/ZS Scripting Library.js
+++ ZS Scripting Library/ZS Scripting Library.js
@@ -220,6 +220,17 @@ function get3dDirection(pos1, pos2)
         return direction;
 }
 
+/** Returns a random number between two values
+* @param {int} min - the minimum number to generate a value from
+* @param {int} max - the minimum number to generate a value from 
+* @param {Boolean} getInt - Only returns integer values if true
+*/
+function getRandom(min, max, getInt)
+{  
+    if(getInt) return Math.floor(Math.random() * (max - min + 1)) + min;
+    else return Math.random() * (max - min + 1) + min;
+}
+
 // GLOBAL CLASSES ------------------------------------------------------------------------------------------------
 
 // Animation Handler class --------------------------------------------------------------------------
