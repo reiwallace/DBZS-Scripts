bd9058af496f9c254b118d69a4538358e158ca8e
Noxie <reiwallace@yahoo.com>
2025-09-10 13:47:46 +0100

    Add sheathe on slot change

--- Project Z Sword/playerSheathe.js
+++ Project Z Sword/playerSheathe.js
@@ -1,5 +1,13 @@
 // Re-sheathe Z Sword on login
-function login(event) {
+function login(event) 
+{
+    var player = event.player;
+    if(lib.removeZSword(player)) lib.giveZSword(player);
+}
+
+// Re-sheathe Z Sword on slot change
+function profileChange(event) 
+{
     var player = event.player;
     if(lib.removeZSword(player)) lib.giveZSword(player);
 }
