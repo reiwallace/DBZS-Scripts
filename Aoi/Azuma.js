d2b0431e07afe58429096ad2bde134529a5f77fe
Noxie <reiwallace@yahoo.com>
2025-09-25 12:53:08 +0100

    Initial deck and azuma

--- Aoi/Azuma.js
+++ Aoi/Azuma.js
@@ -4,7 +4,19 @@ var slashPart = API.createParticle(azumaTexture);
 slashPart.setSize(16, 16);
 slashPart.setMaxAge(40);
 
-function Azuma(npc, target, delay)
+var particlePositions = [
+    [0.5, 2, 0],
+    [1, 1, 0]
+    [0.5, 0, 0]
+]
+
+function azuma(npc, target, delay)
 {
-    slashPart.spawn(npc.world);
+    var npcX = npc.getX();
+    var npcY = npc.getY();
+    var npcZ = npc.getZ();
+    for(var y = 0; y <= 2; y += 0.1) {
+        var x = ((y - 2) ** 2) / 3 + 1
+        npc.spawnParticle("FLAME", npcX + x, npcY + y, npcZ, 0, 0, 0, 0, 1);
+    }
 }
