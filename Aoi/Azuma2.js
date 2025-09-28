8bf08fc0f6816c188782c50a847d342043757a02
Noxie <reiwallace@yahoo.com>
2025-09-28 19:37:51 +0100

    Implement initial particle math

--- Aoi/Azuma2.js
+++ Aoi/Azuma2.js
@@ -1,8 +1,10 @@
 var slashPart = API.createParticle("customnpcs:textures/items/npcWaterElement.png");
-slashPart.setSize(16, 96);
-slashPart.setMaxAge(40);
-//slashPart.setAnim(4, true, 0, 12);
+slashPart.setSize(16, 16);
+slashPart.setAnim(2, true, 1, 200);
 slashPart.setScale(3, 3, 0, 0);
+
+
+var age = 20
 
 // function azuma(npc)
 // {
@@ -28,6 +30,7 @@ function interact(event) {
     var npc = event.npc;
     npc.timers.forceStart(0, 0, true);
     z = maxZ;
+    ticks = 0
 }
 
 function timer(event) {
@@ -38,9 +41,10 @@ function timer(event) {
             npc.timers.clear();
             return;
         }
+        
         azuma(npc, z);
     }
-
+    ticks += 1
 }
 
 function azuma(npc, z)
@@ -49,11 +53,13 @@ function azuma(npc, z)
     var npcY = npc.getY();
     var npcZ = npc.getZ();
     var x = -(Math.pow((z - 1.7), 2)) / 3 + 2
-    var fz = z/2-1.3
+    var fz = z/2.5-1.2
     var y = Math.pow(fz, 3) + Math.pow(fz, 2) + fz*1.8;
-    var density = x*3;
+    var density = Math.pow(x,2)*2;
     for(var i = 0; i < density; i++) {
-        slashPart.setPosition(npcX + x + i/20, npcY + y + 2.5, npcZ + (z/1.3 - maxZ/2.6));
+        slashPart.setMaxAge(age +10 - ticks);
+        slashPart.setAlpha(1, 0, 2, age - ticks);
+        slashPart.setPosition(npcX + x + i/20 + lib.getRandom(-0.0, 0.0), npcY + y + 2 + lib.getRandom(-0.0, 0.0), npcZ + (z/1.3 - maxZ/2.6)) + lib.getRandom(-0.0, 0.0);
         slashPart.spawn(npc.world);
     }
 }
