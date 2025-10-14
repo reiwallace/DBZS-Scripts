5127b05a06a5054dd890102d7fdf8de4b843f8c7
Noxiiie <noxtraditor@gmail.com>
2025-10-14 17:53:30 +0100

    Trim down if statements

--- Godswitch/Player scripts/godswitchHealing.js
+++ Godswitch/Player scripts/godswitchHealing.js
@@ -9,6 +9,7 @@ var heal = {
     fusionBoost : 2.0, //expected as a multiplier, 1.5 would be 50%, 2 is double healing.
     subZeroFail : 0.5 //expected as a percentage, 0.4 is a 40% chance. 0.25% would be a 25% chance.
 }
+var formId = 9;
 
 // TIMERS
 var HEALING_TIMER = 67;
@@ -24,31 +25,31 @@ function dbcFormChange(event) {
 
     //If the player enters SSG, start the healing timer.
     //The player should not need the timer started if it's already ongoing, it will self maintain.
-    if (formInto == 9 && !player.getTimers().has(HEALING_TIMER)) timers.start(HEALING_TIMER, heal.interval, false);
+    if (formInto == formId && !player.getTimers().has(HEALING_TIMER)) timers.start(HEALING_TIMER, heal.interval, false);
 
     //If the player leaves SSG, stop the healing timer
-    if (formFrom == 9) timers.stop(HEALING_TIMER);
+    if (formFrom == formId) timers.stop(HEALING_TIMER);
 }
 
 // Healing Function
 function timer(event) {
-    if (event.id != 67) return;
-
-    //player details
     var player = event.player;
-    if(!lib.isPlayer(player)) return;
-    var dbc = player.getDBCPlayer();
-    if(!dbc || !dbc.getForm() != 9) return; // Additional check in case the player managed to leave ssg without trigger above.
+    var dbc = player ? player.getDBCPlayer() : null;
+    if(event.id != 67 || 
+        !lib.isPlayer(player) || 
+        !dbc || 
+        dbc.getCurrentForm().getID() != formId
+    ) return;
 
     //player stats, relating to HP
     var maxHP = dbc.getMaxHP();
     var currHP = dbc.getBody();
     var healPer = maxHP * heal.amount;
-    player.timers.forceStart(HEALING_TIMER, healingInterval, false); //forcefully restart the timer to continue the healing
+    player.timers.forceStart(HEALING_TIMER, heal.interval, false); //forcefully restart the timer to continue the healing
 
     if(currHP <= 0 && Math.random() < heal.subZeroFail) return; //If the player is currently dying, make a chance for the heal to cancel out so they're not invincible
 
     //Calculate the player's new HP
-    var finalBody = currHP + dbc.getJRMCSE().contains("Z") ? healPer * heal.fusionBoost : healPer;
+    var finalBody = currHP + (dbc.getJRMCSE().contains("Z") ? healPer * heal.fusionBoost : healPer);
     dbc.setBody(finalBody > maxHP ? maxHP : finalBody);
 }
