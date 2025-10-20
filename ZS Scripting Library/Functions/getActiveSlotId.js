478b24f27fe0e4e3e75d170eb53118b817ace1a4
Noxiiie <noxtraditor@gmail.com>
2025-10-20 11:59:10 +0100

    Remove unused library

--- ZS Scripting Library/Functions/getActiveSlotId.js
+++
@@ -1,8 +0,0 @@
-/** Gets a player's current slot id
- * @param {IPlayer} playerName
- * @returns Int
- */
-function getActiveSlotId(player)
-{
-    return API.getProfileHandler().getProfile(player).getCurrentSlotId();
-}
--- ZS Scripting Library/Functions/getProfileData.js
+++
@@ -1,8 +0,0 @@
-/** Gets player profile containing slot data
- * @param {IPlayer} player
- * @returns IProfile
- */
-function getProfileData(player)
-{
-    return API.getProfileHandler().getProfile(player);
-}
