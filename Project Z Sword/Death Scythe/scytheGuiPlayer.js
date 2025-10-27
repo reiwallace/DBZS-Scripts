91c6b8f9f68beec95ba71964ae62db8de4c3f994
reiwallace <reiwallace@yahoo.com>
2025-10-27 23:24:16 +0000

    Rename scytheGuiPlayer and add sounds

--- Project Z Sword/Death Scythe/scytheGuiPlayer.js
+++ Project Z Sword/Death Scythe/scytheGuiPlayer.js
@@ -1,6 +1,14 @@
+// upgradeGuiPlayer.js
+// PLEASE UPDATE ALL SCRIPTS ON GITHUB IF YOU MAKE ANY CHANGES
+// IF YOU REMOVE ANY GLOBAL SCRIPT PLEASE MARK IT AS 'INACTIVE' ON THE GITHUB
+// AUTHOR: Noxie
+
 var scytheGuiId = 227;
 var upgradeButtonId = 3;
 var goonButtonId = 4;
+
+var upgradeSound = API.createSound("minecraft:random.anvil_use");
+var failSound = API.createSound("minecraft:random.anvil_land");
 
 function customGuiSlotClicked(event) {
     if(event.getGui().getID() == scytheGuiId) event.setCancelled(true);
@@ -14,6 +22,14 @@ function customGuiButton(event) {
     if(button.getID() == upgradeButtonId) {
         if(!player.hasTempData("scytheUpgradeFunctions")) return;
         player.getTempData("scytheUpgradeFunctions").apply(player, player.getHeldItem(), player.getTempData("scytheUpgradeFunctions").upgrades);
+        if(player.getTempData("scytheUpgradeFunctions").upgrades.length > 0) {
+            player.closeGui();
+            upgradeSound.setPosition(player.getPosition());
+            API.playSound(upgradeSound);
+        } else {
+            failSound.setPosition(player.getPosition());
+            API.playSound(failSound);
+        }
     } else if(button.getID() == goonButtonId) {
         if(!scytheNpc) return;
         player.interactWith(scytheNpc);
